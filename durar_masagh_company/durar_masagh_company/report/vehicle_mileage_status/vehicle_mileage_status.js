d3d174fb437ad6246ef2d743b269500f08f2e140
Antony Praveenkumar M <antony.bizmap@gmail.com>
2023-07-06 05:22:30 +0000

    vehicle mileage status

--- durar_masagh_company/durar_masagh_company/report/vehicle_mileage_status/vehicle_mileage_status.js
+++ durar_masagh_company/durar_masagh_company/report/vehicle_mileage_status/vehicle_mileage_status.js
@@ -2,19 +2,39 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
+
+var month_dict = {
+	1:'Jan',
+	2:'Feb',
+	3:'Mar',
+	4:'Apr',
+	5:'May',
+	6:'Jun',
+	7:'Jul',
+	8:'Aug',
+	9:'Sep',
+	10:'Oct',
+	11:'Nov',
+	12:'Dec',
+}
+
 frappe.query_reports["Vehicle Mileage Status"] = {
 	"filters": [
 		{
 			fieldname: "vehicle",
 			label: __("Vehicle"),
 			fieldtype: "Link",
-			options: "Vehicle"
+			options: "Vehicle",
+			reqd: 1
 		},
 		{
 			fieldname: "month",
 			label: __("Month"),
 			fieldtype: "Select",
-			options: "\nJan\nFeb"
+			options: "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
+			default: month_dict[frappe.datetime.str_to_obj(frappe.datetime.now_date()).getMonth() + 1]
+			
+		
 		},
 		{
 			fieldname: "from",
