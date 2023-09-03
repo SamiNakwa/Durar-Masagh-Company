40fe1fe29b6b77010ff4628d3834b4096c57e3cb
Antony Praveenkumar M <antony.bizmap@gmail.com>
2023-09-03 22:52:39 +0530

    Employee Checking changes

--- durar_masagh_company/public/js/employee_checkin.js
+++ durar_masagh_company/public/js/employee_checkin.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("Employee Checkin", {
     refresh(frm){
         user_restriction(frm);
         if(frm.is_new()){
-            set_current_user(frm);
+            set_current_user_and_shift(frm);
             create_checkin_and_checkout_button(frm);
         }
 
@@ -35,15 +35,14 @@ const create_checkin_and_checkout_button = (frm) => {
                             }else{
                                 frm.set_value('log_type', 'IN')
                             }
-                            
                             set_current_location(frm)
                 
                         }).addClass(class_name);
 
                     }else{
                         frm.add_custom_button('<i class="fa fa-check"></i> Check-In', () => {
-                            set_current_location(frm)
                             frm.set_value('log_type', 'IN')
+                            set_current_location(frm)
                         }).addClass('btn-primary');
                     }
 
@@ -69,15 +68,24 @@ const user_restriction = (frm) => {
 }
 
 
-const set_current_user = (frm) => {
+const set_current_user_and_shift = (frm) => {
     frappe.call({
-        method: "durar_masagh_company.durar_masagh_company.overrides.employee_checkin.get_current_user_data",
+        method: "durar_masagh_company.durar_masagh_company.overrides.employee_checkin.get_current_user_data_and_shift",
         type: "POST",
-        args: {user_id:frappe.session.user},
+        args: { user_id: frappe.session.user },
+        async: false,
         callback: function(r) {
-            let employee = r.message
-            if(employee){
-                frm.set_value('employee', employee)
+            let data = r.message
+            if(data.employee){
+                frm.set_value('employee', data.employee)
+                if (data.shift_data) {
+                    let shift_data = data.shift_data
+                    frm.set_value('shift', shift_data.shift_type)
+                    frm.set_value('shift_start', shift_data.shift_start)
+                    frm.set_value('shift_end', shift_data.shift_end)
+                    frm.set_value('shift_actual_start', shift_data.shift_actual_start)
+                    frm.set_value('shift_actual_end', shift_data.shift_actual_end)
+                }
             }else{
                 frappe.msgprint({
                     title: __('Notification'),
