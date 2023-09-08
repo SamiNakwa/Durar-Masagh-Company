31885cc89ba0a1a023e93751f44983b97208d01a
Antony Praveenkumar M <antony.bizmap@gmail.com>
2023-09-08 19:17:35 +0000

    employee task details completed

--- durar_masagh_company/durar_masagh_company/doctype/employee_task_management/employee_task_management.py
+++ durar_masagh_company/durar_masagh_company/doctype/employee_task_management/employee_task_management.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2023, Samiulla Nakwa and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class EmployeeTaskManagement(Document):
-	pass
+	
+	@frappe.whitelist()
+	def get_current_employee(self):
+		user = frappe.session.user
+
+		data = frappe.db.get_value('Employee', {'user_id':user}, 'name')
+
+		if data:
+			return {'employee': data}
+		else:
+			return False
